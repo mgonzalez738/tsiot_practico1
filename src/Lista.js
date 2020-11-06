fc6a0da38ebfef7b964559128aee7f0f922175b4
Martin Gonzalez <mgonzalez738@gmail.com>
2020-11-06 19:37:27 -0300

    Prueba 1

---
+++ src/Lista.js
@@ -0,0 +1,9 @@
+module.exports = class Lista {
+
+    constructor() {}
+
+    count() {
+        return 0;
+    }
+    
+}
--- test/test_lista.js
+++ test/test_lista.js
@@ -1,5 +1,7 @@
 const assert = require("chai").assert;
 
+const Lista = require("../src/Lista.js");
+
 describe("en una lista vacia" , function() {
     var lista = new Lista();
     it("hay cero elementos", function() {
